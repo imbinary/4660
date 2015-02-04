2a1279b7405be37bc309ce3a4bd15ca490d59cec
Chris Orem <chrisorem@gmail.com>
2015-02-03 19:22:35 -0500

    working through assignment1

--- pp.py
+++ pp.py
@@ -1,1 +1,11 @@
 __author__ = 'chris'
+from PIL import Image
+
+pil_im = Image.open('empire.jpg')
+pil_im.show()
+
+pil_im_gray = pil_im.convert('L')
+pil_im_gray.show()
+
+pil_im_gray.save("empire_gray.png")
+
