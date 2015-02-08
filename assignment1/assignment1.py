cc7d2040b383f1a2c7c3248d7bede28aa18f5335
Chris Orem <chrisorem@gmail.com>
2015-02-08 12:26:12 -0500

    added loops for both sets

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -9,18 +9,17 @@ def main():
     infiles = ['puzzle_1.jpg', 'puzzle_2.png']
     intemps = ['query_1.jpg', 'query_2.png']
 
-
     for x in range(0, len(infiles)):
 
         # 4 methods for comparison in a list
         methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR', 'cv2.TM_CCORR_NORMED']
 
-
-
         for meth in methods:
             method = eval(meth)
+
             img = cv2.imread(infiles[x], 1)
             t, wi, hi = img.shape[::-1]
+            # make grayscale image for matching
             img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             template = cv2.imread(intemps[x], 0)
             w, h = template.shape[::-1]
@@ -33,27 +32,30 @@ def main():
             top_left = max_loc
             bottom_right = (top_left[0] + w, top_left[1] + h)
 
-            # display plot for saving
+            # display plot
             plt.plot(121), plt.imshow(res, cmap='gray')
             plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
             plt.suptitle(meth)
             plt.show()
 
-            plt.savefig(meth+"_plot_"+infiles[x])
+            # save plot
+            plt.savefig(infiles[x].split('.')[0]+"_plot_"+meth.split('.')[1]+"."+infiles[x].split('.')[1])
+            # plt.clf()
 
             # Draw rectangle on image where the best score is found
             cv2.rectangle(img, top_left, bottom_right, (127, 0, 127), 2)
+
             # Display image using OpenCV
             cv2.namedWindow(meth+"_final_"+infiles[x], cv2.WINDOW_NORMAL)
             if x == 1:
                 cv2.resizeWindow(meth+"_final_"+infiles[x], wi/2, hi/2)
+            else:
+                cv2.resizeWindow(meth+"_final_"+infiles[x], wi, hi)
             cv2.imshow(meth+"_final_"+infiles[x], img)
-
             cv2.waitKey(0)
-            # Save in file using OpenCV
-            cv2.imwrite(meth+"_final_"+infiles[x], img)
-            cv2.destroyAllWindows()
 
+            # Save in file using OpenCV
+            cv2.imwrite(infiles[x].split('.')[0]+"_final_"+meth.split('.')[1]+"."+infiles[x].split('.')[1], img)
 
 
 
