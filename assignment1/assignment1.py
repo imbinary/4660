5bcc2803e29fbbd206c1147d3e6ab30e0958614f
Chris Orem <chrisorem@gmail.com>
2015-02-08 10:02:36 -0500

    added loops for both sets

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -6,48 +6,49 @@ import matplotlib.pyplot as plt
 
 
 def main():
+    infiles = ['puzzle_1.jpg', 'puzzle_2.png']
+    intemps = ['query_1.jpg', 'query_2.png']
 
-    img = cv2.imread('puzzle_1.jpg', 0)
-    img2 = img.copy()
-    template = cv2.imread('query_1.jpg', 0)
-    w, h = template.shape[::-1]
 
-    # All the 6 methods for comparison in a list
-    methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR', 'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+    for x in range(0, len(infiles)):
 
-    for meth in methods:
-        img = img2.copy()
-        method = eval(meth)
+        # 4 methods for comparison in a list
+        methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR', 'cv2.TM_CCORR_NORMED']
 
-        # Apply template Matching
-        res = cv2.matchTemplate(img, template, method)
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+        img = cv2.imread(infiles[x], 1)
+        img2 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        template = cv2.imread(intemps[x], 0)
+        w, h = template.shape[::-1]
 
-        # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-        if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-            top_left = min_loc
-        else:
+        for meth in methods:
+            method = eval(meth)
+
+            # Apply template Matching
+            res = cv2.matchTemplate(img2, template, method)
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+            # find corners of matched image
             top_left = max_loc
-        bottom_right = (top_left[0] + w, top_left[1] + h)
+            bottom_right = (top_left[0] + w, top_left[1] + h)
 
-        cv2.rectangle(img, top_left, bottom_right, 255, 2)
+            # display plot for saving
+            plt.subplot(121), plt.imshow(res, cmap='gray')
+            plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
+            plt.suptitle(meth)
+            # plt.show()
+            plt.imsave(meth+str(x)+infiles[x], res)
 
-        plt.subplot(121), plt.imshow(res, cmap='gray')
-        plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-        plt.subplot(122), plt.imshow(img, cmap='gray')
-        plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-        plt.suptitle(meth)
+            # Draw rectangle on image where the best score is found
+            cv2.rectangle(img, top_left, bottom_right, (127, 0, 127), 2)
+            # Display image using OpenCV
 
-        plt.show()
+            cv2.imshow('here is Waldo - '+meth, img)
+            cv2.waitKey(0)
+            # Save in file using OpenCV
+            cv2.imwrite(meth+" "+infiles[x], img)
+            cv2.destroyAllWindows()
 
 
-    # Draw rectangle on image where the best score is found
-
-    # Display image using OpenCV
-
-    # Save in file using OpenCV
-
-    pass
 
 
 if __name__ == "__main__":
