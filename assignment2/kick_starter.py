65c261cfee4efc92565aef301ca97fdc1719e774
Chris Orem <chrisorem@gmail.com>
2015-03-21 08:40:08 -0400

    tracking

--- assignment2/kick_starter.py
+++ assignment2/kick_starter.py
@@ -8,6 +8,9 @@ from naoqi import ALProxy
 import motion, almath, vision_definitions
 
 
+X = 160/2
+Y = 120/2
+
 def shift_weight(motion_proxy):
     effectors = ["Head", "Torso"]
     axis_masks = [almath.AXIS_MASK_ROT, almath.AXIS_MASK_VEL]
@@ -46,10 +49,60 @@ def getImage(camProxy, camera):
     camProxy.unsubscribe(nameId)
     im = np.array(Image.frombytes("RGB", (naoImage[0], naoImage[1]), naoImage[6]))
     im = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
+
     return im
 
+def findBall(im):
+    ORANGE_MIN = np.array([5, 50, 50], np.uint8)
+    ORANGE_MAX = np.array([15, 255, 255], np.uint8)
+    img2 = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
+    img2 = cv2.inRange(img2, ORANGE_MIN, ORANGE_MAX)
+    mom = cv2.moments(img2)
+    x = -1
+    y = -1
+    if mom["m00"] != 0:
+        y = int(mom["m01"]/mom["m00"])
+        x = int(mom["m10"]/mom["m00"])
 
+    return x, y
 
+def showCam(camProxy):
+    im1 = getImage(camProxy, 0)
+    im2 = getImage(camProxy, 1)
+
+    loc = findBall(im1)
+    if loc[0] == -1:
+        print "no ball"
+    else:
+        cv2.circle(im1, loc, 5, (0, 0, 200))
+        print loc
+        print abs(loc[0]-X)
+
+    im = np.concatenate((im1, im2), axis=0)
+    cv2.imshow("bottom", im)
+    cv2.waitKey(2)
+
+def centerOnBall(motionProxy, camProxy, camera):
+    im1 = getImage(camProxy, camera)
+    loc = findBall(im1)
+    if loc[0] == -1:
+        print "no ball"
+        return
+
+    if abs(loc[0]-X) < 2:
+        return
+    motionProxy.wakeUp()
+    #postureProxy.goToPosture("StandInit", 0.5)
+    motionProxy.moveInit()
+    motionProxy.moveTo(0, 0, -.2)
+    if loc[0]-X < 0:
+        # turn left
+        print "turning left"
+        motionProxy.moveTo(0, 0, -.2)
+    else:
+        # turn right
+        print "turning right"
+        motionProxy.moveTo(0, 0, .2)
 
 def main():
     pip = "127.0.0.1"
@@ -59,23 +112,25 @@ def main():
 
     # -------------------------------------------
     # YOUR CODE HERE
+
     # setup additional proxies
     camProxy = ALProxy("ALVideoDevice", pip, pport)
     postureProxy = ALProxy("ALRobotPosture", pip, pport)
 
-    im = getImage(camProxy, 0)
-    cv2.imshow("bottom", im)
-    cv2.waitKey(500)
+    # initialize motion
+    #motionProxy.wakeUp()
+    #postureProxy.goToPosture("StandInit", 0.5)
+    #motionProxy.moveInit()
 
-    motionProxy.wakeUp()
-    postureProxy.goToPosture("StandInit", 0.5)
-    motionProxy.moveInit()
-    motionProxy.moveTo(1, 0.332, 0)
+    # look for ball
+    #while 1:
+        #centerOnBall(motionProxy, camProxy, 0)
 
-    postureProxy.goToPosture("StandInit", 0.5)
+    centerOnBall(motionProxy, camProxy, 0)
+    #postureProxy.goToPosture("StandInit", 0.5)
     # print motion_proxy.getSummary()
     # YOUR CODE END
 
-    kick(motionProxy)
+    #kick(motionProxy)
 if __name__ == "__main__":
     main()
