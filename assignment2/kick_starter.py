3c2b412a35711aa9df1d5668927eb02003998447
Chris Orem <chrisorem@gmail.com>
2015-03-21 16:51:27 -0400

    diff

--- assignment2/kick_starter.py
+++ assignment2/kick_starter.py
@@ -117,7 +117,11 @@ def moveforward(loc, camera, motionProxy):
     dist = 0.6
     print loc[1]
     if camera == 1:
-        dist = 0.15
+        dist = 0.3
+        if loc[0] > 80:
+            dist = .15
+        if loc[1] > 110:
+            dist = .75
 
     motionProxy.moveTo(dist, 0, 0)
 
